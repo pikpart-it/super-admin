41126bce5a13c0bbb5afbe3ecb08a2abdf16e245
afoda111 <afoda1327@gmail.com>
2024-10-22 13:22:32 +0300

    merged satbeer changes, new SuperAdmin UAT build 10/22/2024

---
+++ dist/assets/Dashboard-OLBhTb-d.js
@@ -0,0 +1,1 @@
+import{j as r,F as e}from"./index-2Plsp2nQ.js";const s=()=>r.jsx(r.Fragment,{children:r.jsx(e,{justifyContentCenter:!0,children:r.jsx("div",{children:"Dashboard"})})});export{s as default};
