a5a1a3fd278ac4da82bedb8ae9fcfdea9e41f65f
afoda111 <afoda1327@gmail.com>
2024-11-25 11:53:03 +0200

    Merged Satbeer changes, new superAdmin UAT build 11/25/2024

--- dist/assets/CreateUsers-BeGgLaAe.js
+++ dist/assets/CreateUsers-BeGgLaAe.js
@@ -1,4 +1,4 @@
-import{p as C,u as z,r as p,e as V,j as r,F as f,v as X,a as Y,B as Z,g as w,b as D,c as ee,d as k}from"./index-BCvnNA56.js";import{F as b,M as B}from"./Form-D655TDoo.js";import{P as ne,L as se,M as re}from"./AddBUForm-BcRh5dO3.js";import"./switchClasses-MrYpXv-n.js";C.div`
+import{p as C,u as z,r as p,e as V,j as r,F as f,v as X,a as Y,B as Z,g as w,b as D,c as ee,d as k}from"./index-Xh-l5RVz.js";import{F as b,M as B}from"./Form-CbFc0wZI.js";import{P as ne,L as se,M as re}from"./AddBUForm-VRXBLvqX.js";import"./switchClasses-D2jzv0Nb.js";C.div`
   width: 80%;
   margin: auto;
 `;C.div`
---
+++ dist/assets/Dashboard-RO-o94bJ.js
@@ -0,0 +1,1 @@
+import{j as r,F as e}from"./index-Xh-l5RVz.js";const s=()=>r.jsx(r.Fragment,{children:r.jsx(e,{justifyContentCenter:!0,children:r.jsx("div",{children:"Dashboard"})})});export{s as default};
